df356034c06c566b32a036285c93edbfa5821570
klobodnf <admin@Waleys-Mac-Pro.local>
2014-07-10 14:36:08 +0800

    支持A星寻路了

--- Done/DoneScripts/PlayerScripts/DonePlayerMovement.cs
+++ Done/DoneScripts/PlayerScripts/DonePlayerMovement.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using Pathfinding;
 
 public class DonePlayerMovement : MonoBehaviour
 {
@@ -10,10 +11,34 @@ public class DonePlayerMovement : MonoBehaviour
 	
 	private Animator anim;				// Reference to the animator component.
 	private DoneHashIDs hash;			// Reference to the HashIDs.
+
+
+	//The point to move to
+	public Transform targetPosition;
 	
+	private Seeker seeker;
+	
+	//The calculated path
+	public Path path;
+	
+	//The AI's speed per second
+	public float speed = 100;
+	
+	//The max distance from the AI to a waypoint for it to continue to the next waypoint
+	public float nextWaypointDistance = 3;
+	
+	//The waypoint we are currently moving towards
+	private int currentWaypoint = 0;
+
 	
 	void Awake ()
 	{
+
+		seeker = GetComponent<Seeker>();
+		
+		//Start a new path to the targetPosition, return the result to the OnPathComplete function
+		seeker.StartPath (transform.position,targetPosition.localPosition, OnPathComplete);
+
 		// Setting up the references.
 		anim = GetComponent<Animator>();
 		hash = GameObject.FindGameObjectWithTag(DoneTags.gameController).GetComponent<DoneHashIDs>();
@@ -31,6 +56,9 @@ public class DonePlayerMovement : MonoBehaviour
 		bool sneak = Input.GetButton("Sneak");
 		
 		MovementManagement(h, v, sneak);
+
+
+		MoveToPath ();
 	}
 	
 	
@@ -98,5 +126,42 @@ public class DonePlayerMovement : MonoBehaviour
 		if(shout)
 			// ... play the shouting clip where we are.
 			AudioSource.PlayClipAtPoint(shoutingClip, transform.position);
+	}
+
+
+
+
+
+	
+	public void OnPathComplete (Path p) {
+		Debug.Log ("Yay, we got a path back. Did it have an error? "+p.error);
+		if (!p.error) {
+			path = p;
+			//Reset the waypoint counter
+			currentWaypoint = 0;
+		}
+	}
+	
+	private void MoveToPath () {
+		if (path == null) {
+			//We have no path to move after yet
+			return;
+		}
+		
+		if (currentWaypoint >= path.vectorPath.Count) {
+			Debug.Log ("End Of Path Reached");
+			return;
+		}
+		
+		//Direction to the next waypoint
+		Vector3 dir = (path.vectorPath[currentWaypoint]-transform.position).normalized;
+		dir *= speed * Time.fixedDeltaTime;
+		
+		//Check if we are close enough to the next waypoint
+		//If we are, proceed to follow the next waypoint
+		if (Vector3.Distance (transform.position,path.vectorPath[currentWaypoint]) < nextWaypointDistance) {
+			currentWaypoint++;
+			return;
+		}
 	}
 }
